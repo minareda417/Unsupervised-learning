a32d6c0036f5d50d991a9f2f0a134b71e356425c
omar-hekal <137761235+omar-hekal@users.noreply.github.com>
2025-12-19 16:05:46 +0200

    add customizable activation functions: Sigmoid, ReLU, and Tanh

---
+++ autoencoder/activations.py
@@ -0,0 +1,33 @@
+from abc import ABC, abstractmethod
+import numpy as np
+
+class ActivationFunction(ABC):
+    @abstractmethod
+    def activate(self, x):
+        pass
+
+    @abstractmethod
+    def derivative(self, x):
+        pass
+
+class SigmoidActivation(ActivationFunction):
+    def activate(self, x):
+        return 1 / (1 + np.exp(-x))
+    
+    def derivative(self, x):
+        s = self.activate(x)
+        return s * (1 - s)
+    
+class ReLUActivation(ActivationFunction):
+    def activate(self, x):
+        return np.maximum(0, x)
+    
+    def derivative(self, x):
+        return np.where(x > 0, 1, 0)
+    
+class TanhActivation(ActivationFunction):
+    def activate(self, x):
+        return np.tanh(x)
+    
+    def derivative(self, x):
+        return 1 - np.tanh(x)**2
